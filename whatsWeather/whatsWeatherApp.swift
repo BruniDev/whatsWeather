fedb52873c4112f6d76b36ed2c99efc9d85a167b
Junghyeon Kim <hyeondxx@gmail.com>
2023-04-26 23:38:08 +0900

    Initial Commit

---
+++ whatsWeather/whatsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  whatsWeatherApp.swift
+//  whatsWeather
+//
+//  Created by 정현 on 2023/04/26.
+//
+
+import SwiftUI
+
+@main
+struct whatsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
